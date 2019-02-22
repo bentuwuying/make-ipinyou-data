84e975a698b1a804de49946538f5e02c79dbc71d
dingyujian <dingyujian@dingyujiandeMacBook-Pro.local>
2019-02-22 15:57:14 +0800

    macos

--- mkyzxdata.sh
+++ mkyzxdata.sh
@@ -1,7 +1,7 @@
 advertisers="1458 2261 2997 3386 3476 2259 2821 3358 3427"
 
-for advertiser in $advertisers; do
+for advertiser in `echo $advertisers`; do
     echo $advertiser
-    python python/mkyzx.py $advertiser/train.log.txt $advertiser/test.log.txt $advertiser/train.yzx.txt $advertiser/test.yzx.txt $advertiser/featindex.txt
+    python2.7 python/mkyzx.py $advertiser/train.log.txt $advertiser/test.log.txt $advertiser/train.yzx.txt $advertiser/test.yzx.txt $advertiser/featindex.txt
 done
 
